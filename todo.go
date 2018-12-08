bbed3c8c0eeb5c7ee7293819378a1cfc64f95971
James Wilson <jgwilson1214@gmail.com>
2018-12-08 00:44:22 -0800

    write new todo to a file

--- todo.go
+++ todo.go
@@ -1,20 +1,20 @@
 //TODO lol
-// actually change todo.txt when adding or removing
 // shift todos after removing
+// actually change todo.txt when removing
 // do something about map iteration order
-// create todo.txt if it doesn't exist?
 package main
 
 import (
 	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
-var addPtr = flag.String("a", "null", "add todo") // -a adds a todo item.
-var removePtr = flag.Int("r", 0, "remove todo")   // -r removes a todo item.
-var todos map[int]string                          // Maps todos as keys (int) to values (string).
+var addPtr = flag.String("a", "null", "add todo") // -a adds a todo item
+var removePtr = flag.Int("r", 0, "remove todo")   // -r removes a todo item
+var todos map[int]string                          // Maps todos as keys (int) to values (string)
 
 func display(m map[int]string) {
 	// Print entire todo list
@@ -25,26 +25,36 @@ func display(m map[int]string) {
 
 func main() {
 	flag.Parse()
-	// Read todo.txt into todos map.
-	f, err := os.Open("todo.txt")
-	if err == nil {
-		todos = make(map[int]string)
-		scanner := bufio.NewScanner(f)
-		// Loop over all lines in the file and store them as todos.
-		for i := 1; scanner.Scan(); i++ {
-			line := scanner.Text()
-			todos[i] = line
-		}
+	// If todo.txt file doesn't exist, create it, or append to the file
+	f, err := os.OpenFile("todo.txt", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	todos = make(map[int]string)
+	scanner := bufio.NewScanner(f)
+	// Loop over all lines in the file and store them as todos
+	for i := 1; scanner.Scan(); i++ {
+		line := scanner.Text()
+		todos[i] = line
 	}
 	if *addPtr != "null" {
-		// Add todo.
+		// Add todo
 		fmt.Println("add: ", *addPtr)
 		todos[len(todos)+1] = *addPtr
+		if _, err := f.WriteString(*addPtr + "\n"); err != nil {
+			log.Fatal(err)
+		}
 	} else if *removePtr != 0 {
-		// Remove todo.
+		// Remove todo
 		fmt.Println("remove: ", *removePtr)
 		delete(todos, *removePtr)
+		//TODO shift map key/values
+		// update todo.txt
 	} else {
 		display(todos)
 	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+
 }
